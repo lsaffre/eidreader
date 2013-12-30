88a8a6c668737aa08a9d054ea54103ef01958480
Luc Saffre <luc.saffre@gmail.com>
2013-12-30 06:48:15 +0200

    http://docs.lino-framework.org/blog/2013/1230.html

--- applets/get_jars.sh
+++ applets/get_jars.sh
@@ -6,10 +6,11 @@ mv eid-applet-sdk-1.1.0.GA/eid-applet-service-1.1.0.GA.jar eid-applet-service.ja
 rm -R eid-applet-sdk-1.1.0.GA
 rm eid-applet-sdk-1.1.0.GA.zip
 
-wget http://archive.apache.org/dist/commons/codec/binaries/commons-logging-1.1.3-bin.tar.gz
+wget http://archive.apache.org/dist/commons/logging/binaries/commons-logging-1.1.3-bin.tar.gz
 tar -xvzf commons-logging-1.1.3-bin.tar.gz 
 mv commons-logging-1.1.3/commons-logging-1.1.3.jar commons-logging.jar
 rm -R commons-logging-1.1.3
+rm commons-logging-1.1.3-bin.tar.gz
 
 wget http://archive.apache.org/dist/commons/codec/binaries/commons-codec-1.8-bin.tar.gz
 tar -xvzf commons-codec-1.8-bin.tar.gz
