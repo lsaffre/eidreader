de7feafd5b6a307d82d6f4b49a5c3cc07a0e573e
Luc Saffre <luc.saffre@gmail.com>
2014-07-02 05:19:17 +0300

    http://docs.lino-framework.org/blog/2014/0702.html

--- pavement.py
+++ pavement.py
@@ -30,21 +30,11 @@ def update(src, dst):
         shutil.copy2(src, dst)
 
 
-def build_jar(alias):
-    outdir = path('example') / alias
+def sign_jars(outdir, alias, flags):
     jarfile = outdir / JARFILE
     m = max([f.mtime for f in CLASSES])
-    # m = None
-    # for fn in CLASSES:
-    #     if m is None or fn.mtime > m:
-    #         m = fn.mtime
     if not jarfile.exists() or jarfile.mtime < m:
         sh("jar cvfm %s %s" % (jarfile, ' '.join(CLASSES)))
-
-
-def sign_jars(alias, flags):
-    outdir = path('example') / alias
-    jarfile = outdir / JARFILE
     sh("jarsigner %s %s %s" % (flags, jarfile, alias))
     for n in LIBJARS:
         jarfile = outdir / n
@@ -54,7 +44,6 @@ def sign_jars(alias, flags):
 
 @task
 def classes():
-    # CLASSPATH = "example/eid-applet-service.jar:example/commons-codec.jar"
     JFLAGS = "-Xlint:unchecked -classpath %s" % CLASSPATH
     for src in SOURCES:
         sh("javac %s %s" % (JFLAGS, src))
@@ -64,43 +53,16 @@ def classes():
 @needs('classes')
 def jars():
     flags = '-storepass "`cat ~/.secret/.keystore_password`"'
-    build_jar('mykey')
-    sign_jars('mykey', flags)
+    sign_jars(path('example'), 'mykey', flags)
 
     flags += ' -tsa http://timestamp.globalsign.com/scripts/timestamp.dll'
-    build_jar('codegears')
-    sign_jars('codegears', flags)
+    sign_jars(path('example/signed'), 'codegears', flags)
 
 
 
 """
-
-jars_mykey: classes
-	export SIGNERFLAGS = -storepass "`cat ~/.secret/.keystore_password`"
-	export ALIAS = mykey
-	export JARDIR = example/$(ALIAS)
-	jar cvfm $(JARDIR)/$(JARFILE) $(OBJECTS)
-	jarsigner $(SIGNERFLAGS) $(JARDIR)/$(JARFILE) $(ALIAS)
-	jarsigner $(SIGNERFLAGS) $(JARDIR)/eid-applet-service.jar $(ALIAS)
-	jarsigner $(SIGNERFLAGS) $(JARDIR)/commons-codec.jar $(ALIAS)
-	jarsigner $(SIGNERFLAGS) $(JARDIR)/commons-logging.jar $(ALIAS)
-
-jars_codegears: classes
-	export SIGNERFLAGS=-tsa http://timestamp.globalsign.com/scripts/timestamp.dll -storepass "`cat ~/.secret/.keystore_password`"
-	export ALIAS = codegears
-	export JARDIR = example/$(ALIAS)s
-	jar cvfm $(JARDIR)/$(JARFILE) $(OBJECTS)
-	jarsigner $(SIGNERFLAGS) $(JARDIR)/$(JARFILE) $(ALIAS)
-	jarsigner $(SIGNERFLAGS) $(JARDIR)/eid-applet-service.jar $(ALIAS)
-	jarsigner $(SIGNERFLAGS) $(JARDIR)/commons-codec.jar $(ALIAS)
-	jarsigner $(SIGNERFLAGS) $(JARDIR)/commons-logging.jar $(ALIAS)
-
 clean:
 	rm -f src/eidreader/*.class
-	rm -f example/mykey/*.jar
-	rm -f example/codegears/*.jar
-
-xpi:
-	cd firefox/eidreader ; make xpi
+	rm -f example/signed/*.jar
 
 """
